e84487fa444062a301245efc36870b479cc43fa5
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2019-09-28 19:39:43 -0400

    More package changes

--- StoriesForSustainability/e2e/src/app.e2e-spec.ts
+++ StoriesForSustainability/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to survey-central!');
+    expect(page.getParagraphText()).toEqual('Welcome to Stories for Sustainability!');
   });
 });
